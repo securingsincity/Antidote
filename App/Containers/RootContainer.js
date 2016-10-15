419f17ac9e175c58b0c62c04da1ebae04cf0db18
James Hrisho <james.hrisho@gmail.com>
2016-10-15 10:13:23 -0400

    Stop tracking certain files

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -6,6 +6,7 @@ import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
 // import './Config/PushConfig'
 
+import ReactNativeUA from 'react-native-ua'; // import <module></module>
 // Styles
 import styles from './Styles/RootContainerStyle'
 
@@ -15,6 +16,32 @@ class RootContainer extends Component {
     if (!ReduxPersist.active) {
       this.props.startup()
     }
+
+    ReactNativeUA.enable_notification(); // prompt user to enable notification
+
+    ReactNativeUA.enable_geolocation(); // prompt user to enable geolocation
+
+    ReactNativeUA.enable_action_url(); // enable url action
+
+    ReactNativeUA.handle_background_notification(); // handle notifications when app is in background
+
+    ReactNativeUA.add_tag('tag'); // set tag to the user
+
+    ReactNativeUA.set_named_user_id('user_id'); // set named user id
+  }
+
+  componentWillMount() {
+      // add handler to handle all incoming notifications
+      ReactNativeUA.on_notification((notification) => {
+          console.log('notification:',
+                      notification.url, // if action url is disabled
+                      notification.platform,
+                      notification.event,
+                      notification.alert,
+                      notification.data);
+
+          alert(notification.alert);
+      });
   }
 
   render () {
