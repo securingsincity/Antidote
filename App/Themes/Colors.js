8435edca042333d219ecfddb4f459e1172471ffc
Cory <cory.noonan@gmail.com>
2016-10-22 16:59:46 +0200

    welcome screen updates

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,5 +1,5 @@
 const colors = {
-  background: '#1F0808',
+  background: '#F2F1F1',
   clear: 'rgba(0,0,0,0)',
   facebook: '#3b5998',
   transparent: 'rgba(0,0,0,0)',
@@ -17,7 +17,7 @@ const colors = {
   snow: 'white',
   ember: 'rgba(164, 0, 48, 0.5)',
   fire: '#e73536',
-  drawer: 'rgba(30, 30, 29, 0.95)'
+  drawer: 'rgba(30, 30, 29, 0.95)',
 }
 
 export default colors
